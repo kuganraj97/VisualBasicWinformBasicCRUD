9d7a462bf33b5cb5c2d523bda7b98a7dbe06c949
kuganraj97 <75333632+kuganraj97@users.noreply.github.com>
2022-06-22 10:35:10 +0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,269 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.NameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.IDDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.AddressDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ContactDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TableCustomerBindingSource = New System.Windows.Forms.BindingSource()
+        Me.CustomerDataSet = New WindowsApplication1.CustomerDataSet()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Table_CustomerTableAdapter = New WindowsApplication1.CustomerDataSetTableAdapters.Table_CustomerTableAdapter()
+        Me.CustomerDataSet1 = New WindowsApplication1.CustomerDataSet()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TableCustomerBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CustomerDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CustomerDataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Lime
+        Me.Button1.Location = New System.Drawing.Point(52, 245)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Add"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Gold
+        Me.Button2.Location = New System.Drawing.Point(157, 245)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Update"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.Red
+        Me.Button3.Location = New System.Drawing.Point(275, 245)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Delete"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(50, 67)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(50, 101)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(18, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "ID"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(49, 136)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(45, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Address"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(49, 194)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(44, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Contact"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(136, 67)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(214, 20)
+        Me.TextBox1.TabIndex = 9
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(136, 101)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(214, 20)
+        Me.TextBox2.TabIndex = 10
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(136, 136)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(214, 49)
+        Me.TextBox3.TabIndex = 11
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(136, 191)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(214, 20)
+        Me.TextBox4.TabIndex = 12
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.NameDataGridViewTextBoxColumn, Me.IDDataGridViewTextBoxColumn, Me.AddressDataGridViewTextBoxColumn, Me.ContactDataGridViewTextBoxColumn})
+        Me.DataGridView1.DataSource = Me.TableCustomerBindingSource
+        Me.DataGridView1.Location = New System.Drawing.Point(441, 80)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(471, 206)
+        Me.DataGridView1.TabIndex = 13
+        '
+        'NameDataGridViewTextBoxColumn
+        '
+        Me.NameDataGridViewTextBoxColumn.DataPropertyName = "Name"
+        Me.NameDataGridViewTextBoxColumn.HeaderText = "Name"
+        Me.NameDataGridViewTextBoxColumn.Name = "NameDataGridViewTextBoxColumn"
+        '
+        'IDDataGridViewTextBoxColumn
+        '
+        Me.IDDataGridViewTextBoxColumn.DataPropertyName = "ID"
+        Me.IDDataGridViewTextBoxColumn.HeaderText = "ID"
+        Me.IDDataGridViewTextBoxColumn.Name = "IDDataGridViewTextBoxColumn"
+        '
+        'AddressDataGridViewTextBoxColumn
+        '
+        Me.AddressDataGridViewTextBoxColumn.DataPropertyName = "Address"
+        Me.AddressDataGridViewTextBoxColumn.HeaderText = "Address"
+        Me.AddressDataGridViewTextBoxColumn.Name = "AddressDataGridViewTextBoxColumn"
+        '
+        'ContactDataGridViewTextBoxColumn
+        '
+        Me.ContactDataGridViewTextBoxColumn.DataPropertyName = "Contact"
+        Me.ContactDataGridViewTextBoxColumn.HeaderText = "Contact"
+        Me.ContactDataGridViewTextBoxColumn.Name = "ContactDataGridViewTextBoxColumn"
+        '
+        'TableCustomerBindingSource
+        '
+        Me.TableCustomerBindingSource.DataMember = "Table_Customer"
+        Me.TableCustomerBindingSource.DataSource = Me.CustomerDataSet
+        '
+        'CustomerDataSet
+        '
+        Me.CustomerDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.Button5.Location = New System.Drawing.Point(852, 33)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(60, 25)
+        Me.Button5.TabIndex = 16
+        Me.Button5.Text = "Search"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Table_CustomerTableAdapter
+        '
+        Me.Table_CustomerTableAdapter.ClearBeforeFill = True
+        '
+        'CustomerDataSet1
+        '
+        Me.CustomerDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(441, 36)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(389, 20)
+        Me.TextBox5.TabIndex = 17
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(949, 319)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TableCustomerBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CustomerDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CustomerDataSet1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents CustomerDataSet As WindowsApplication1.CustomerDataSet
+    Friend WithEvents TableCustomerBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents Table_CustomerTableAdapter As WindowsApplication1.CustomerDataSetTableAdapters.Table_CustomerTableAdapter
+    Friend WithEvents NameDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents IDDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents AddressDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents ContactDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents CustomerDataSet1 As WindowsApplication1.CustomerDataSet
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,239 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+     Dim connection As SqlConnection = New SqlConnection("Data Source=DESKTOP-PIHCL6D\MYSQLSERVER2;Initial Catalog=Customer;Integrated Security=True")
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Load_data()
+    End Sub
+
+    'Load data Function.
+    Private Sub Load_data()
+        Me.Text = "Profile"
+        Dim command As New SqlCommand("Select * from Table_Customer",connection)
+        Dim sda As New SqlDataAdapter(command)
+        Dim dt As New DataTable
+        sda.Fill(dt)
+        DataGridView1.DataSource = dt
+    End Sub
+
+    'Add Function.
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+
+        Try
+            Dim Name As String = TextBox1.Text
+            Dim Address As String = TextBox3.Text
+            Dim Contact As String = TextBox4.Text
+            Dim ID As Integer = TextBox2.Text
+
+            'Length Validator
+            If TextBox4.Text.Length < 7 Then
+                MsgBox("Phone numbers must be at least 7 digits long")
+                TextBox4.Focus()
+
+             ElseIf TextBox4.Text.Length > 12 Then
+                MsgBox("Phone numbers must be of a maximum of 12 digits long")
+                TextBox4.Focus()
+            Else
+
+
+                connection.Open()
+                Dim command As New SqlCommand("Insert into Table_Customer  VALUES ('" & Name & "','" & ID & "','" & Address & "','" & Contact & "')", connection)
+                command.ExecuteNonQuery()
+                TextBox1.Clear()
+                TextBox2.Clear()
+                TextBox3.Clear()
+                TextBox4.Clear()
+                connection.Close()
+
+                Load_data()
+                MsgBox("Added Successfully", Title:="Message")
+            End If
+        Catch ex As InvalidCastException
+            MsgBox("Please input complete information to Add", Title:="Message")
+        End Try
+    End Sub
+
+    'Update Function.
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        Try
+
+            Dim Name As String = TextBox1.Text
+            Dim ID As Integer = TextBox2.Text
+            Dim Address As String = TextBox3.Text
+            Dim Contact As String = TextBox4.Text
+
+            'Empty Fields Validator
+            If (TextBox1.Text = "" Or TextBox2.Text = "" Or TextBox3.Text = "" Or TextBox4.Text = "") Then
+                TextBox1.Clear()
+                TextBox2.Clear()
+                TextBox3.Clear()
+                TextBox4.Clear()
+                MessageBox.Show("NULL OR EMPTY")
+
+            ElseIf (TextBox1.Text = "" And TextBox2.Text = "" And TextBox3.Text = "" And TextBox4.Text = "") Then
+                MessageBox.Show("NULL OR EMPTY")
+
+                'Length Validator
+            ElseIf TextBox4.Text.Length < 7 Then
+                MsgBox("Phone numbers must be at least 7 digits long")
+                TextBox4.Focus()
+
+            ElseIf TextBox4.Text.Length > 12 Then
+                MsgBox("Phone numbers must be of a maximum of 12 digits long")
+                TextBox4.Focus()
+            Else
+                connection.Open()
+                Dim command As New SqlCommand("Update Table_Customer SET Name ='" & Name & "', Address = '" & Address & "', Contact = '" & Contact & "' where ID = '" & ID & "'", connection)
+                command.ExecuteNonQuery()
+                TextBox1.Clear()
+                TextBox2.Clear()
+                TextBox3.Clear()
+                TextBox4.Clear()
+                connection.Close()
+
+                Load_data()
+                MsgBox("Updated Successfully", Title:="Message")
+            End If
+        Catch ex As InvalidCastException
+            MessageBox.Show("NULL OR EMPTY")
+
+        End Try
+    End Sub
+
+
+    'Delete Function.
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+        Try
+            Dim ID As Integer = TextBox2.Text
+
+            connection.Open()
+            Dim command As New SqlCommand("delete Table_Customer where ID = '" & ID & "'", connection)
+            command.ExecuteNonQuery()
+            TextBox1.Clear()
+            TextBox2.Clear()
+            TextBox3.Clear()
+            TextBox4.Clear()
+            connection.Close()
+
+            Load_data()
+        Catch ex As InvalidCastException
+            MsgBox("Please Input ID to delete", Title:="Message")
+        End Try
+    End Sub
+
+
+    'Search Function
+    Private Sub Button5_Click(sender As System.Object, e As System.EventArgs) Handles Button5.Click
+        Dim searchtxt As String = TextBox5.Text
+
+        connection.Open()
+        Dim command As New SqlCommand("Select * from Table_Customer WHERE Name LIKE '%" + searchtxt + "%' or ID LIKE '%" + searchtxt + "%' or Contact LIKE '%" + searchtxt + "%' or Address LIKE '%" + searchtxt + "%'", connection)
+        command.ExecuteNonQuery()
+        Dim sda As New SqlDataAdapter(command)
+        Dim dt As New DataTable
+        sda.Fill(dt)
+        DataGridView1.DataSource = dt
+        connection.Close()
+    End Sub
+
+
+    'Validator Function
+    Private Sub Textbox2_keypress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox2.KeyPress
+
+
+        'Number Validator
+        If e.KeyChar <> ChrW(Keys.Back) Then
+            If Char.IsNumber(e.KeyChar) Then
+            Else
+                MessageBox.Show("Invalid Input! Numbers Only.", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                e.Handled = True
+            End If
+        End If
+
+
+        'Length Validator
+        If TextBox2.Text.Length >= 4 Then
+            If e.KeyChar <> ControlChars.Back Then
+                MessageBox.Show("Invalid Input! 4 Digits Only.", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                TextBox2.Clear()
+                e.Handled = True
+            End If
+        End If
+       
+    End Sub
+
+
+    Private Sub Textbox1_keypress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox1.KeyPress
+
+        'Number Validator
+        If e.KeyChar <> ChrW(Keys.Back) Then
+            If Char.IsNumber(e.KeyChar) Then
+                MessageBox.Show("Invalid Input! Alphabets Only.", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                TextBox1.Clear()
+                e.Handled = True
+            End If
+        End If
+
+        If e.KeyChar <> ChrW(Keys.Back) Then
+            If Char.IsPunctuation(e.KeyChar) Then
+                MessageBox.Show("Invalid Input! Alphabets Only.", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                TextBox1.Clear()
+                e.Handled = True
+            End If
+        End If
+
+        'Length Validator
+        If TextBox1.Text.Length >= 50 Then
+            If e.KeyChar <> ControlChars.Back Then
+                MessageBox.Show("Invalid Input! 50 Characters Only.", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                TextBox1.Clear()
+                e.Handled = True
+            End If
+        End If
+
+        'First Letter Caps
+        Static PreviousLetter As Char
+        If PreviousLetter = " "c Or TextBox1.Text.Length = 0 Then
+            e.KeyChar = Char.ToUpper(e.KeyChar)
+        End If
+        PreviousLetter = e.KeyChar
+       
+    End Sub
+
+
+    Private Sub Textbox3_keypress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox3.KeyPress
+        If TextBox3.Text.Length >= 250 Then
+            If e.KeyChar <> ControlChars.Back Then
+                MessageBox.Show("Invalid Input! 250 Characters Only.", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                TextBox3.Clear()
+                e.Handled = True
+            End If
+        End If
+
+        'First Letter Caps
+        Static PreviousLetter As Char
+        If PreviousLetter = " "c Or TextBox3.Text.Length = 0 Then
+            e.KeyChar = Char.ToUpper(e.KeyChar)
+        End If
+        PreviousLetter = e.KeyChar
+
+   
+    End Sub
+
+    'Validator Function
+    Private Sub Textbox4_keypress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox4.KeyPress
+        'Number Validator
+        If e.KeyChar <> ChrW(Keys.Back) Then
+            If Char.IsNumber(e.KeyChar) Then
+            Else
+                MessageBox.Show("Invalid Input! Numbers Only.", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                e.Handled = True
+            End If
+        End If
+    End Sub
+
+    
+    
+End Class
+
